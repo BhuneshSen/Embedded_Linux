49648323def5716ccbc5fa848b167c08f09e474e
Bhunesh Sen <bhuneshsen761@gmail.com>
2025-12-09 22:51:33 +0530

    Add files via upload
    unidirectional and bidirectional Pipe IPC

---
+++ 03_IPCs/02_Bidirectional_Pipe/02_Pipe_Bidirectional.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(){
+	
+	printf("q1.c Initiated...\n");
+	
+	int fd[2];
+	int fd2[2];									// file descriptor 
+	char buffer[30];
+	
+	if(pipe(fd) == -1 || pipe(fd2) == -1){
+		printf("Pipe Failed... \n");
+	}
+	
+	pid_t pid = fork();
+	
+	if(pid < 0){
+		perror("fork failed..\n");
+	}
+
+	if( pid != 0){
+//		printf("Parent Process \n");
+		close(fd[0]);
+		char msg[] = "(P2C)Hello Child from parent";	// Parent to Child
+		write(fd[1], msg, strlen(msg)+1);		// sending data to adjusent process
+		close(fd[1]);
+		
+		close(fd2[1]);
+		read(fd2[0], buffer, sizeof(buffer));
+		close(fd2[0]);
+		
+		printf("Parent Process: %s \n", buffer);
+	}
+	else{
+//		printf("Child Process \n");
+		close(fd[1]);							// close Read write end
+		read(fd[0], buffer, sizeof(buffer));	// reading data send by adjusent parent process
+		close(fd[0]);
+		printf("child Process: %s \n", buffer);
+	
+		
+		char msg[] = "(C2P)Hello parent from child";	// child to parent
+		
+		close(fd2[0]);
+		write(fd2[1], msg, strlen(msg));
+		close(fd2[1]);
+		
+		
+	}
+	
+	printf("End of Process %d \n", getpid());
+	return 0;
+}
+
+
+/*
+int fd[2] - fd is file descriptor
+
+fd[0] - read end;
+fd[1] - write end;
+
+pipe(fd) - function get arr of 2 int (as mention above), return -1 if fail to set-up communication
+
+
+*/
